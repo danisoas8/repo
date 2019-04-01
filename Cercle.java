ffa3a836b3d5d66481bd2613fb6fc77db4221ff6
dsoto <dani.soas8@gmail.com>
2019-04-01 11:37:47 +0200

    cercle funcions

--- Cercle.java
+++ Cercle.java
@@ -8,6 +8,22 @@ public class Cercle {
             y=yy;
         }
 
+	public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setX(int x) {
+            this.x=x;
+        }
+
+        public void setY(int y) {
+            this.y=y;
+        }
+
         Cercle(){
             x=0;
             y=0;
